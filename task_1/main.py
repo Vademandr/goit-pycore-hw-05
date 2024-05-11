916bd4fc3e07730f0277796ef0ad2044f4106e03
Vademandr <vademandrpro@gmail.com>
2024-05-12 00:50:05 +0300

    create t2

--- task_1/main.py
+++ task_1/main.py
@@ -2,12 +2,16 @@ def caching_fibonacci() -> callable:
     cache = {}  
 
     def fibonacci(n: int) -> int:
+        # Якщо n <= 0, повертаємо 0
         if n <= 0:
             return 0
+        # Якщо n == 1, повертаємо 1
         elif n == 1:
             return 1
+         # Якщо n вже є у кеші, повертаємо його значення
         elif n in cache:
             return cache[n]
+        # Якщо число не знаходиться у кеші, обчислюємо його рекурсивно
         else:
             cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
             return cache[n]
